540e1c04c54716661ae9566b34addeb5dd1493b9
sparda65 <56416937+sparda65@users.noreply.github.com>
2024-05-06 10:27:27 -0600

    Update dashboardAlumno.blade.php

--- resources/views/includes/dashboardAlumno.blade.php
+++ resources/views/includes/dashboardAlumno.blade.php
@@ -8,46 +8,49 @@
 @if(count($pedidos) == 0)
 No tienes prestamos 
 @else
-<table class="table table-hover">
-    <thead>
-        <tr>
-            <th scope="col">id</th>
-            <th scope="col">Fecha Prestamo</th>
-            <th scope="col">Fecha Entrega</th>
-            <th scope="col">Estado</th>
-            <th scope="col">Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($pedidos as $pedido)
+<div class="table-responsive">
+    
+    <table class="table table-hover">
+        <thead>
             <tr>
-                <td>
-                    {{ $pedido->pedido_id }}
-                </td>
-                <td>
-                    {{ $pedido->pedido_fec_pre }}
-                </td>
-                <td>
-                    @if($pedido->pedido_fec_ent == null)
-                    <div>-</div>
-                    @else
-                    {{$pedido->pedido_fec_ent}}
-                    @endif
-                </td>
-                <td>
-                    @if($pedido->pedido_estado == '1')
-                    Pendiente
-                    @else
-                    Entregado
-                    @endif
-                </td>
-                <td>
-                    <a href="{{ route('pedidoDetalle', [ 'id'=>$pedido->pedido_id.'-'.$pedido->pedido_user_id ]) }}" class="btn btn-info">
-                        Detalle
-                    </a>
-                </td>
+                <th scope="col">id</th>
+                <th scope="col">Fecha Prestamo</th>
+                <th scope="col">Fecha Entrega</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Acciones</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach($pedidos as $pedido)
+                <tr>
+                    <td>
+                        {{ $pedido->pedido_id }}
+                    </td>
+                    <td>
+                        {{ $pedido->pedido_fec_pre }}
+                    </td>
+                    <td>
+                        @if($pedido->pedido_fec_ent == null)
+                        <div>-</div>
+                        @else
+                        {{$pedido->pedido_fec_ent}}
+                        @endif
+                    </td>
+                    <td>
+                        @if($pedido->pedido_estado == '1')
+                        Pendiente
+                        @else
+                        Entregado
+                        @endif
+                    </td>
+                    <td>
+                        <a href="{{ route('pedidoDetalle', [ 'id'=>$pedido->pedido_id.'-'.$pedido->pedido_user_id ]) }}" class="btn btn-info">
+                            Detalle
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endif
